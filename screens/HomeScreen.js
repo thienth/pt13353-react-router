53db2acbcd38b630a23c7f9d3082a75dd3860a9d
thienth32 <thienth32@gmail.com>
2019-01-25 10:26:06 +0700

    update webview indicator modal

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -5,6 +5,10 @@ import {
   Text,
   TouchableOpacity,
   View,
+  WebView,
+  Modal,
+  TouchableHighlight,
+  ActivityIndicator
 } from 'react-native';
 import firebaseConf from '../helpers/firebase';
 
@@ -13,7 +17,9 @@ export default class HomeScreen extends React.Component {
     super(props);
     this.getListPosts = this.getListPosts.bind(this)
     this.state ={
-      posts: []
+      posts: [],
+      modalVisible: false,
+      selectedPost: null
     }
   }
 
@@ -23,7 +29,14 @@ export default class HomeScreen extends React.Component {
   componentDidMount = () => {
     this.getListPosts();
   }
-  
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+    var that = this;
+    setTimeout(() => {
+      that.setState({modalVisible: false});
+    }, 3000);
+  }
 
   getListPosts(){
     var that = this;
@@ -40,26 +53,63 @@ export default class HomeScreen extends React.Component {
       
       that.setState({posts});
   });
-  }
+}
 
-  
+  // const {navigate} = this.props.navigation;
+    // return (
+    //   <View style={styles.container}>
+    //     {this.state.posts.map(row => 
+
+    //         <View key={row.key}>
+    //           <TouchableOpacity
+    //             onPress={() => navigate('Detail', {data: row})}  
+    //           >
+    //             <Image source={{uri: row.data.image}} style={{width: 100, height: 100}}/>
+    //             <Text>{row.data.name}</Text>
+    //           </TouchableOpacity>
+    //         </View>
+    //       )}
+    //   </View>
 
   render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View style={styles.container}>
-        {this.state.posts.map(row => 
+    
+      return (
+        <View style={{marginTop: 20}}>
+              <Modal
+              animationType="slide"
+              transparent={false}
+              visible={this.state.modalVisible}
+              onRequestClose={() => {
+                Alert.alert('Modal has been closed.');
+              }}>
+              <View style={{ flex: 1, 
+                              alignItems: 'center', 
+                              justifyContent: 'center',  
+                              marginTop: 22, 
+                              backgroundColor: 'rgba(52, 52, 52, 0.1)'}}>
+                <View style={{ width: '80%', 
+                                minHeight: 400,
+                                backgroundColor: '#ccc'}}>
+                  <ActivityIndicator size="large" color="#0000ff" />
+                  <Text>Hello World!</Text>
 
-            <View key={row.key}>
-              <TouchableOpacity
-                onPress={() => navigate('Detail', {data: row})}  
-              >
-                <Image source={{uri: row.data.image}} style={{width: 100, height: 100}}/>
-                <Text>{row.data.name}</Text>
-              </TouchableOpacity>
-            </View>
-          )}
-      </View>
+                  <TouchableHighlight
+                    onPress={() => {
+                      this.setModalVisible(!this.state.modalVisible);
+                    }}>
+                    <Text>Hide Modal</Text>
+                  </TouchableHighlight>
+                </View>
+              </View>
+            </Modal>
+
+            <TouchableHighlight
+              onPress={() => {
+                this.setModalVisible(true);
+              }}>
+              <Text>Show Modal</Text>
+            </TouchableHighlight>
+        </View>
     );
   }
 }
