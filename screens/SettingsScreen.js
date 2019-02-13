32af8c7c9c5e5d7ce5d5b0e8851137d7ea124f62
thienth32 <thienth32@gmail.com>
2019-02-13 10:10:51 +0700

    update form style

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {ScrollView, Text, TextInput, StyleSheet,
-        Picker} from 'react-native';
+        Picker, TouchableOpacity} from 'react-native';
 
 export default class SettingsScreen extends React.Component {
   static navigationOptions = {
@@ -12,6 +12,9 @@ export default class SettingsScreen extends React.Component {
     this.state = {
       title: "",
       category: 2,
+      image: "",
+      short_desc: "",
+      content: "",
       listCates: [
         {
           key: 1,
@@ -47,6 +50,23 @@ export default class SettingsScreen extends React.Component {
             <Picker.Item key={item.key} label={item.name} value={item.key}/>  
           )}
         </Picker>
+        <Text style={styles.label}>Đường dẫn ảnh</Text>
+        <TextInput 
+          style={styles.txtInput}
+          onChange={(txt) => {this.setState({image: txt})}} />
+        <Text style={styles.label}>Mô tả ngắn</Text>
+        <TextInput 
+          style={[styles.txtInput, styles.txtarea]}
+          multiline={true}
+          onChange={(txt) => {this.setState({short_desc: txt})}} />
+        <Text style={styles.label}>Nội dung bài viêt</Text>
+        <TextInput 
+          style={[styles.txtInput, styles.txtarea]}
+          multiline={true}
+          onChange={(txt) => {this.setState({content: txt})}} />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTxt}>Lưu</Text>
+        </TouchableOpacity>
       </ScrollView>
 
     )
@@ -67,6 +87,21 @@ const styles = StyleSheet.create({
     marginLeft: 20,
     fontWeight: 'bold',
     marginTop: 10
+  },
+  txtarea:{
+    height: 100
+  },
+  btn:{
+    backgroundColor: '#F0A52B',
+    marginTop: 20,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTxt: {
+    fontWeight: 'bold',
+    color: "#fff",
+    fontSize: 22
   }
 
 });
